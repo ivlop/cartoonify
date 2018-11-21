a082e9329423d816ffce5ddcd4023e6d1bafb555
Patricia <patricia.rodriugez.lastres@udc.es>
2018-11-21 20:11:49 +0100

    implementacion servidor 	modificado:     cartoonify/lib/model.dart 	modificado:     cartoonify/pubspec.yaml

--- cartoonify/lib/model.dart
+++ cartoonify/lib/model.dart
@@ -4,6 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:cartoonify/pagesPicker/transformedImageScreen.dart';
+import 'dart:convert';
+import  'dart:io';
+import 'package:http/http.dart' as http;
+import "package:path/path.dart" show dirname;
+
 
 class AppModel extends Model{
 
@@ -12,8 +17,55 @@ class AppModel extends Model{
   File get image => _image;
 
   void getImage() async {
-    var image = await ImagePicker.pickImage(source: ImageSource.camera);
-    _image = image;
+    var photo = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    // Getting the absolute path to this script file
+    var currentPath = dirname(Platform.script.path);
+    // Generating  absolute paths for the input/output images
+    var pathImage="$currentPath/../samples/image.jpg";
+    var pathCartoon="$currentPath/../samples/cartoon.png";
+
+    // Reading image
+    var image = photo;
+    var imageAsBytes = await image.readAsBytes();
+
+    // Creating request
+    // NOTE: In the emulator, localhost ip is 10.0.2.2
+    var uri = Uri.parse('http://192.168.43.38:5000/cartoon');
+    var request = http.MultipartRequest("POST", uri);
+    var inputFile = http.MultipartFile.fromBytes('image', imageAsBytes, filename: 'image.jpg');
+    request.files.add(inputFile);
+
+    try {
+      // Sending request and waiting for response
+
+      var response = await request.send();
+
+      if (response.statusCode == 200) {
+        // Receiving response stream
+        var responseStr = await response.stream.bytesToString();
+
+        // Converting response string to json dictionary
+        var data = jsonDecode(responseStr);
+
+        // Accessing response data
+        var cartoon = data['cartoon'];
+
+        if (cartoon != null) {
+          // Creating the output file
+          var outputFile = File(pathCartoon);
+          // Decoding base64 string received as response
+          var imageResponse = base64.decode(cartoon);
+          // Writing the decoded image to the output file
+          await outputFile.writeAsBytes(imageResponse);
+
+          _image = outputFile;
+        }
+      }
+    } catch  (e) {
+      print( 'Server is down');
+    }
+
     notifyListeners();
   }
   void deletePressed() {
